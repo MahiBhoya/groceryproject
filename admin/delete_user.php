f92bca9713fa1038bb41e76f0ed4ebc668fe82d0
Bhoyamukta08 <muktabenbhoya19@gnu.ac.in>
2024-10-20 12:06:32 +0530

    user del

--- admin/delete_user.php
+++ admin/delete_user.php
@@ -1,11 +1,18 @@
 <?php
-    if(isset($_GET['delete_users'])){
-        $delete_id = $_GET['delete_users'];
-        $delete_query = "DELETE FROM `user_table` WHERE user_id = $delete_id";
-        $delete_result = mysqli_query($con,$delete_query);
-        if($delete_result){
-            echo "<script>window.alert('user deleted successfully');</script>";
-            echo "<script>window.open('index.php?list_users','_self');</script>";
-        }
+if (isset($_GET['delete_users'])) {
+    $delete_id = $_GET['delete_users'];
+    
+    $delete_query = "DELETE FROM `user_table` WHERE user_id = $delete_id";
+    $delete_result = mysqli_query($con, $delete_query);
+    
+    if ($delete_result) {
+        $delete_orders_query = "DELETE FROM `user_orders` WHERE user_id = $delete_id";
+        $delete_orders_result = mysqli_query($con, $delete_orders_query);
+        
+        echo "<script>window.alert('User and their orders deleted successfully');</script>";
+        echo "<script>window.open('index.php?list_users', '_self');</script>";
+    } else {
+        echo "<script>window.alert('Error deleting user.');</script>";
     }
+}
 ?>
